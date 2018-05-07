97339c69b4ed1df28d5dd544e76361fd8569b5d5
Tianli Bi <citytianyu@gmail.com>
2018-05-07 00:49:38 -0700

    Initial Submit

---
+++ CharacterStatistics/CharacterStatistics/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CharacterStatistics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnSelectPhraseFiles = new System.Windows.Forms.Button();
+            this.openFileDialogPhrase = new System.Windows.Forms.OpenFileDialog();
+            this.btnAnalyzePhrase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "分卷字";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "txt";
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "选择文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(139, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "分卷话";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(139, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "分卷";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnSelectPhraseFiles
+            // 
+            this.btnSelectPhraseFiles.Location = new System.Drawing.Point(28, 172);
+            this.btnSelectPhraseFiles.Name = "btnSelectPhraseFiles";
+            this.btnSelectPhraseFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPhraseFiles.TabIndex = 4;
+            this.btnSelectPhraseFiles.Text = "选择分词";
+            this.btnSelectPhraseFiles.UseVisualStyleBackColor = true;
+            this.btnSelectPhraseFiles.Click += new System.EventHandler(this.btnSelectPhraseFiles_Click);
+            // 
+            // openFileDialogPhrase
+            // 
+            this.openFileDialogPhrase.DefaultExt = "*.txt";
+            this.openFileDialogPhrase.FileName = "openFileDialogPhrase";
+            this.openFileDialogPhrase.Multiselect = true;
+            // 
+            // btnAnalyzePhrase
+            // 
+            this.btnAnalyzePhrase.Location = new System.Drawing.Point(139, 172);
+            this.btnAnalyzePhrase.Name = "btnAnalyzePhrase";
+            this.btnAnalyzePhrase.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyzePhrase.TabIndex = 5;
+            this.btnAnalyzePhrase.Text = "分析分词";
+            this.btnAnalyzePhrase.UseVisualStyleBackColor = true;
+            this.btnAnalyzePhrase.Click += new System.EventHandler(this.btnAnalyzePhrase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 241);
+            this.Controls.Add(this.btnAnalyzePhrase);
+            this.Controls.Add(this.btnSelectPhraseFiles);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnSelectPhraseFiles;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPhrase;
+        private System.Windows.Forms.Button btnAnalyzePhrase;
+    }
+}
+
---
+++ CharacterStatistics/CharacterStatistics/Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterStatistics
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.FileName == "")
+                MessageBox.Show("No file has been selected.");
+            else
+            {
+                if (currFilename.CompareTo(openFileDialog.FileName) != 0)
+                {
+                    // read all the content from the file
+                    string fileContent = System.IO.File.ReadAllText(openFileDialog.FileName, Encoding.UTF8);
+                    // split the text into volume
+                    volumeContents = splitVolume(openFileDialog.FileName, fileContent);
+
+                    currFilename = openFileDialog.FileName;
+                }
+
+                int fileNum = 1;
+                foreach (string content in volumeContents)
+                {
+                    countChar(currFilename, content, fileNum.ToString());
+                }
+
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            //MessageBox.Show(openFileDialog.FileName);
+        }
+
+        private List<string> splitVolume(string filename, string fileContent)
+        {
+            string keyWord = "第一章";
+
+            List<string> subStrings = new List<string>();
+
+            int index = fileContent.IndexOf(keyWord);
+
+            int nextIndex = index + 3;
+            do
+            {
+                nextIndex = fileContent.IndexOf(keyWord, index + 3);
+
+                if (nextIndex == -1)
+                    nextIndex = fileContent.Length;
+
+                string subString = fileContent.Substring(index, nextIndex - index);
+
+                subStrings.Add(subString);
+
+                index = nextIndex;
+
+            } while (nextIndex != -1 && nextIndex < fileContent.Length);
+
+            return subStrings;
+        }
+
+        private void countChar(string filename, string fileContent, string fileIndex)
+        {
+            Dictionary<char, uint> table = new Dictionary<char, uint>();
+            table.Clear();
+
+            for (int i = 0; i < fileContent.Length; i++)
+            {
+                char curr = fileContent[i];
+                uint num = 0;
+                if (table.TryGetValue(curr, out num))
+                {
+                    num++;
+                    table[curr] = num;
+                }
+                else
+                {
+                    table.Add(curr, 1);
+                }
+            }
+
+            uint sum = 0;
+            foreach (KeyValuePair<char, uint> pair in table)
+            {
+                sum += pair.Value;
+            }
+
+            var items = from pair in table
+                        orderby pair.Value descending
+                        select pair;
+
+            string result = "";
+            foreach (KeyValuePair<char, uint> pair in items)
+            {
+                if (pair.Key != ' ')
+                {
+                    float ratio = (float)pair.Value / (float)sum * 100.0f;
+                    result += pair.Key + "  " + pair.Value.ToString() + " " + ratio.ToString() + "%\n";
+                }
+            }
+
+            saveToFile(filename, result, "Result", fileIndex);
+        }
+
+        private void saveToFile(string filename, string result, string name, string num)
+        {
+            string resultFilename = filename;
+            int lastSlashIdx = resultFilename.LastIndexOf('\\');
+            resultFilename = resultFilename.Substring(0, lastSlashIdx + 1);
+            //MessageBox.Show(resultFilename);
+            resultFilename += name + num + ".txt";
+
+            System.IO.File.WriteAllText(resultFilename, result);
+        }
+
+        List<string> volumeContents = null;
+        string currFilename = "";
+
+        Dictionary<string, int>[] MegaTable = new Dictionary<string, int>[1];
+        Dictionary<string, int>[] AccTable = new Dictionary<string, int>[1];
+        int[] Sums = new int[1];
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.FileName == "")
+                MessageBox.Show("No file has been selected.");
+            else
+            {
+                if (currFilename.CompareTo(openFileDialog.FileName) != 0)
+                {
+                    // read all the content from the file
+                    string fileContent = System.IO.File.ReadAllText(openFileDialog.FileName, Encoding.UTF8);
+                    // split the text into volume
+                    volumeContents = splitVolume(openFileDialog.FileName, fileContent);
+
+                    currFilename = openFileDialog.FileName;
+                }
+
+                string result = "";
+                for (int i = 0; i < volumeContents.Count; ++i)
+                {
+                    result += (i + 1).ToString() + " " + countDialogs(volumeContents[i]) + "\n";
+                }
+
+                saveToFile(currFilename, result, "dialogStats", "1");
+
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private int countDialogs(string content)
+        {
+            int left = 0;
+            int right = left;
+
+            int sum = 0;
+            left = content.IndexOf('“');
+
+            do
+            {
+                int alterLeft = content.IndexOf('“', left + 1);
+                right = content.IndexOf('”', left + 1);
+
+                if (right == -1)
+                    break;
+
+                if (alterLeft < right)
+                {
+                    left = alterLeft;
+                    continue;
+                }
+
+                sum += right - left - 1;
+                left = content.IndexOf('“', right + 1);
+            }
+            while (left != -1 && left < content.Length);
+
+            return sum;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.FileName == "")
+                MessageBox.Show("No file has been selected.");
+            else
+            {
+                if (currFilename.CompareTo(openFileDialog.FileName) != 0)
+                {
+                    // read all the content from the file
+                    string fileContent = System.IO.File.ReadAllText(openFileDialog.FileName, Encoding.UTF8);
+                    // split the text into volume
+                    volumeContents = splitVolume(openFileDialog.FileName, fileContent);
+
+                    currFilename = openFileDialog.FileName;
+                }
+
+                string toReplace = "\r\n";
+
+                for (int i = 0; i < volumeContents.Count; ++i)
+                {
+                    string temp = volumeContents[i].Replace(toReplace, " ");
+                    saveToFile(currFilename, temp, "split\\volume", (i + 1).ToString());
+                }
+
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void btnSelectPhraseFiles_Click(object sender, EventArgs e)
+        {
+            openFileDialogPhrase.ShowDialog();
+        }
+
+        private void btnAnalyzePhrase_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogPhrase.FileNames.Length == 0)
+            {
+                MessageBox.Show("No Phrase file has been selected.");
+                return;
+            }
+
+            string[] filenames = openFileDialogPhrase.FileNames;
+
+            Dictionary<int, int> table = new Dictionary<int, int>();
+
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                int number = -1;
+                int startIndex = filenames[i].LastIndexOf('\\') + 1;
+                string substr = filenames[i].Substring(startIndex, filenames[i].LastIndexOf('.') - startIndex);
+                if (int.TryParse(substr, out number))
+                {
+                    table.Add(number, i);
+                }
+            }
+
+            var items = from pair in table
+                        orderby pair.Key ascending
+                        select pair;
+
+  
+            MegaTable = new Dictionary<string, int>[table.Count];
+            AccTable = new Dictionary<string, int>[table.Count];
+            Sums = new int[table.Count];
+
+            int volumeIndex = 0;
+            foreach (KeyValuePair<int, int> pair in items)
+            {
+                string fileContent = System.IO.File.ReadAllText(filenames[pair.Value], Encoding.UTF8);
+
+                MegaTable[volumeIndex] = countPhrase(filenames[pair.Value], fileContent, pair.Key.ToString(), out Sums[volumeIndex]);
+
+                accumulatePhrases(filenames[pair.Value], pair.Key.ToString(), volumeIndex - 1, volumeIndex);
+
+                volumeIndex++;
+            }
+
+            MessageBox.Show("Done!");
+        }
+
+        private Dictionary<string, int> countPhrase(string filename, string content, string fileIndex, out int totalPhraseCount)
+        {
+            Dictionary<string, int> table = new Dictionary<string, int>();
+
+            for (int p = 0; p < content.Length;)
+            {
+                if (content[p] == '\t')
+                {
+                    p++;
+                    continue;
+                }
+
+                int q = p + 1;
+                while (q < content.Length && content[q] != '\t')
+                    q++;
+
+                if (q >= content.Length)
+                    break;
+
+                string phrase = content.Substring(p, q - p);
+                if (table.ContainsKey(phrase))
+                    table[phrase]++;
+                else
+                    table.Add(phrase, 1);
+
+                p = q + 1;
+            }
+
+            string result = "";
+
+            var items = from pair in table
+                        orderby pair.Value descending
+                        select pair;
+
+            int sum = 0;
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                sum += pair.Value;
+            }
+
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                float ratio = (float)pair.Value / (float)sum * 100.0f;
+                result += pair.Key + "\t" + pair.Value.ToString() + "\t" + ratio.ToString() + "%\n";
+            }
+
+            saveToFile(filename, result, "phraseStats", fileIndex);
+
+            totalPhraseCount = sum;
+
+            return table;
+        }
+
+        private void accumulatePhrases(string filename, string fileIndex, int prevAccTableIndex, int currTableIndex)
+        {
+            if (AccTable[currTableIndex] == null)
+                AccTable[currTableIndex] = new Dictionary<string, int>();
+
+            int sum = 0;
+            if (currTableIndex == 0)
+            {
+                MegaTable[0].ToList().ForEach(x => AccTable[0].Add(x.Key, x.Value));
+                sum = Sums[currTableIndex];
+            }
+            else
+            {
+                // copy prev to currAcc
+                foreach (KeyValuePair<string, int> pair in AccTable[prevAccTableIndex])
+                {
+                    AccTable[currTableIndex].Add(pair.Key, pair.Value);
+                }
+
+                // accumulate
+                foreach (KeyValuePair<string, int> pair in MegaTable[currTableIndex])
+                {
+                    if (AccTable[currTableIndex].ContainsKey(pair.Key))
+                        AccTable[currTableIndex][pair.Key] += pair.Value;
+                    else
+                        AccTable[currTableIndex].Add(pair.Key, pair.Value);
+                }
+
+                for (int j = 0; j <= prevAccTableIndex; j++)
+                    sum += Sums[j];
+                sum += Sums[currTableIndex];
+            }
+
+            var items = from pair in AccTable[currTableIndex]
+                        orderby pair.Value descending
+                        select pair;
+
+            string result = "";
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                float ratio = (float)pair.Value / (float)sum * 100.0f;
+                result += pair.Key + "\t" + pair.Value.ToString() + "\t" + ratio.ToString() + "%\n";
+            }
+
+            saveToFile(filename, result, "AccPhraseStats", fileIndex);
+        }
+    }
+}
